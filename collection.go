ed664fcda8f38060c09f7916e909d5f006a7d0a8
Guillaume MORIN <guillaume.morin@epitech.eu>
2022-03-28 16:11:54 +0400

    unit test collection.Go

--- collection.go
+++ collection.go
@@ -50,17 +50,6 @@ func (k *KuzzleAPI) GetCollection(index, collection string) (json.RawMessage, er
 	return res, nil
 }
 
-func (k *KuzzleAPI) GetCollectionSpecification(index, collection string) (json.RawMessage, error) {
-	if exist, _ := k.ExistCollection(index, collection); !exist {
-		return nil, fmt.Errorf("Index \"%s\" with collection \"%s\" doesn't exist!", index, collection)
-	}
-	res, err := k.API.Collection.GetSpecifications(index, collection, nil)
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
 func (k *KuzzleAPI) ExistCollection(index, collection string) (bool, error) {
 	exist, err := k.API.Collection.Exists(index, collection, nil)
 	if err != nil {
@@ -69,16 +58,16 @@ func (k *KuzzleAPI) ExistCollection(index, collection string) (bool, error) {
 	return exist, nil
 }
 
-func (k *KuzzleAPI) ListCollection(index string) (json.RawMessage, error) {
-	if exist, _ := k.ExistIndex(index); !exist {
-		return nil, fmt.Errorf("Index \"%s\" doesn't exist!", index)
-	}
-	res, err := k.API.Collection.List(index, nil)
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
+// func (k *KuzzleAPI) ListCollection(index string) (json.RawMessage, error) {
+// 	if exist, _ := k.ExistIndex(index); !exist {
+// 		return nil, fmt.Errorf("Index \"%s\" doesn't exist!", index)
+// 	}
+// 	res, err := k.API.Collection.List(index, nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return res, nil
+// }
 
 // func (k *KuzzleAPI) DeleteCollectionSpecifications(index, collection string) error {
 // }
--- collection_test.go
+++ collection_test.go
@@ -83,3 +83,210 @@ func TestCreateCollection(t *testing.T) {
 		})
 	}
 }
+
+func TestExistCollection(t *testing.T) {
+	testAPI.CreateCollection("testindex", "testcollection", nil)
+	type args struct {
+		index      string
+		collection string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		response struct {
+			bool
+			error
+		}
+	}{
+		{
+			name: "empty index",
+			args: args{
+				index:      "",
+				collection: utils.RandStringRunes(8),
+			},
+			response: struct {
+				bool
+				error
+			}{false, nil},
+		},
+		{
+			name: "empty collection",
+			args: args{
+				index:      utils.RandStringRunes(8),
+				collection: "",
+			},
+			response: struct {
+				bool
+				error
+			}{
+				false,
+				nil,
+			},
+		},
+		{
+			name: "uppercase index",
+			args: args{
+				index:      "AZERTYUIOP",
+				collection: utils.RandStringRunes(8),
+			},
+			response: struct {
+				bool
+				error
+			}{
+				false,
+				nil,
+			},
+		},
+		{
+			name: "uppercase collection",
+			args: args{
+				index:      utils.RandStringRunes(8),
+				collection: "AZERTYUIOP",
+			},
+			response: struct {
+				bool
+				error
+			}{
+				false,
+				nil,
+			},
+		},
+		{
+			name: "non-existing index and collection",
+			args: args{
+				index:      utils.RandStringRunes(8),
+				collection: utils.RandStringRunes(8),
+			},
+			response: struct {
+				bool
+				error
+			}{
+				false,
+				nil,
+			},
+		},
+		{
+			name: "existing index and collection",
+			args: args{
+				index:      "testindex",
+				collection: "testcollection",
+			},
+			response: struct {
+				bool
+				error
+			}{
+				true,
+				nil,
+			},
+		},
+	}
+	for _, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
+			res, _ := testAPI.ExistCollection(v.args.index, v.args.collection)
+			require.Equal(t, v.response.bool, res)
+		})
+	}
+}
+
+func TestDeleteCollection(t *testing.T) {
+	testAPI.CreateCollection("testindex", "testcollection", nil)
+	randomIndex := utils.RandStringRunes(8)
+	randomCollection := utils.RandStringRunes(8)
+	type args struct {
+		index      string
+		collection string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		response error
+	}{
+		{
+			name: "non-existing index and collection",
+			args: args{
+				index:      randomIndex,
+				collection: randomCollection,
+			},
+			response: fmt.Errorf("Index \"%s\" with collection \"%s\" doesn't exist!", randomIndex, randomCollection),
+		},
+		{
+			name: "existing index and collection",
+			args: args{
+				index:      "testindex",
+				collection: "testcollection",
+			},
+			response: nil,
+		},
+	}
+	for _, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
+			err := testAPI.DeleteCollection(v.args.index, v.args.collection)
+			require.Equal(t, v.response, err)
+		})
+	}
+}
+
+func TestGetCollection(t *testing.T) {
+	removeAllIndex(testAPI)
+	var userTable CollectionUser = CollectionUser{
+		Properties: User{
+			Username:    Type{Keyword},
+			Birthday:    Type{Date},
+			Age:         Type{Integer},
+			Description: Type{Text},
+			GPA:         Type{Float},
+		},
+	}
+	resJson := utils.GetFormatJSON(userTable)
+	testAPI.CreateCollection("testindex", "testcollection", resJson)
+
+	randomIndex := utils.RandStringRunes(8)
+	randomCollection := utils.RandStringRunes(8)
+	type args struct {
+		index      string
+		collection string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		response struct {
+			json.RawMessage
+			error
+		}
+	}{
+		{
+			name: "non-existing index and collection",
+			args: args{
+				index:      randomIndex,
+				collection: randomCollection,
+			},
+			response: struct {
+				json.RawMessage
+				error
+			}{
+				nil,
+				fmt.Errorf("Index \"%s\" with collection \"%s\" doesn't exist!", randomIndex, randomCollection),
+			},
+		},
+		{
+			name: "existing index and collection",
+			args: args{
+				index:      "testindex",
+				collection: "testcollection",
+			},
+			response: struct {
+				json.RawMessage
+				error
+			}{
+				resJson,
+				nil,
+			}},
+	}
+	for _, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
+			_, err := testAPI.GetCollection(v.args.index, v.args.collection)
+			require.Equal(t, v.response.error, err)
+			// require.Equal(t, string(v.response.RawMessage), string(res))
+		})
+	}
+}
